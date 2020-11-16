cd50a023341c020f840fa3fa8e9aa07f2a91ed1d
MBSinterns000 <56572066+MBSinterns000@users.noreply.github.com>
2020-11-15 19:05:42 -0500

    Soundboard files

---
+++ soundboard.js
@@ -0,0 +1,21 @@
+/*$(document).ready(function(){
+	$('button').on('click', function(){
+  	var id = this.id.slice(-1);
+    //console.log(this.id.slice(-1));
+    document.getElementById('#sound' + id).play();
+  });
+
+});*/
+
+function playSound1() {
+    var sound1 = document.getElementById("sound1");
+    sound1.play();
+}
+function playSound2() {
+    var sound2 = document.getElementById("sound2");
+    sound2.play();
+}
+function playSound3() {
+    var sound3 = document.getElementById("sound3");
+    sound3.play();
+}
